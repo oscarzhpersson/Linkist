e08f512c8d2af8bcef18956b36771226236cd16f
Oscar Persson <oscar.persson@klarna.com>
2024-04-13 14:22:04 +0200

    Adds sleep endpoint to external api

--- backend/api/dummy-external-api.py
+++ backend/api/dummy-external-api.py
@@ -1,7 +1,12 @@
 from flask import Flask, jsonify
 import subprocess
+import random
+
+app = Flask(__name__)
+
 
 @app.route('/sleep', methods=['GET'])
 def sleep():
-    subprocess.run(["sleep", "10"])
-    return jsonify({"message": "Slept for 10 seconds"})
+    random_number = random.randint(1, 5)
+    subprocess.run(["sleep", str(random_number)])
+    return jsonify({f"message": f"Slept for {random_number} seconds"})
